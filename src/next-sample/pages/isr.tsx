a6660af99cade1e6151cb1db9184f73fff31b771
yasudaProduct <yasu.private.ct@gmail.com>
2022-08-18 19:54:10 +0900

    3.8.6

---
+++ src/next-sample/pages/isr.tsx
@@ -0,0 +1,48 @@
+import { GetStaticProps, NextPage } from "next"
+import Head from "next/head"
+import { useRouter } from "next/router"
+
+
+type ISRProps = {
+    message: string
+}
+
+//ISPropsを受け付けるNextPage(ページ)の型
+const ISR: NextPage<ISRProps> = (props) => {
+    const { message } = props
+
+    const router = useRouter()
+
+    if (router.isFallback) {
+        //フォールバック用のページを返す
+        return <div>Loading...</div>
+    }
+
+    return (
+        <div>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <p>このページはISRによってビルド時に生成されたページです</p>
+                <p>{message}</p>
+            </main>
+        </div>
+    )
+}
+
+export const getStaticProps: GetStaticProps<ISRProps> = async (context) => {
+    const timestamp = new Date().toLocaleDateString()
+    const message = `${timestamp} にこのページのgetStaticPropsが実行されました`
+
+    return {
+        props: {
+            message,
+        },
+        // ページの有効期間を秒単位で指定
+        revalidate: 60,
+    }
+}
+
+export default ISR
